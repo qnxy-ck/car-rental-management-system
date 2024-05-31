06dc41180480a5842968d83437a76a8b47d8911d
qnxy <qnxy1997@gmail.com>
2024-06-01 02:07:32 +0800

    登录页业务逻辑

---
+++ src/com/qnxy/service/LoginPageService.java
@@ -0,0 +1,23 @@
+package com.qnxy.service;
+
+import com.qnxy.common.LoginType;
+
+/**
+ * 登录页
+ *
+ * @author Qnxy
+ */
+public class LoginPageService {
+
+
+    public boolean login(LoginType loginType, String username, String password) {
+        return false;
+    }
+
+
+    public boolean register(LoginType loginType, String username, String password) {
+        return false;
+    }
+
+
+}
